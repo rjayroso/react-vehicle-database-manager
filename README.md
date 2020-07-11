8218293fad2e8c4ffed05595b7419b3893d9a75d
Royce Ayroso-Ong <rjayroso-ong@myseneca.ca>
2020-07-11 04:13:45 -0400

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-=======
-# react-vehicle-database-manager
->>>>>>> 15f29c6fbafc22bd4c93163c520f8fc14744e601
---
+++ precache-manifest.2cc5f91222702e4171334d172495a423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ffabd70791346c6b64428a94cb26c04",
+    "url": "/react-vehicle-database-manager/index.html"
+  },
+  {
+    "revision": "d4f4baec3169e0ef144b",
+    "url": "/react-vehicle-database-manager/static/css/main.3bbfbf58.chunk.css"
+  },
+  {
+    "revision": "23ff42c45080e1fc786d",
+    "url": "/react-vehicle-database-manager/static/js/2.599d7cbb.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-vehicle-database-manager/static/js/2.599d7cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4f4baec3169e0ef144b",
+    "url": "/react-vehicle-database-manager/static/js/main.5630f12c.chunk.js"
+  },
+  {
+    "revision": "9e13a44ef5a52041c768",
+    "url": "/react-vehicle-database-manager/static/js/runtime-main.c26cbea6.js"
+  }
+]);
